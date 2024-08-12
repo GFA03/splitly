f0ab325a1503117088567b165c830df0ead5f8ff
Gavrila Alexandru <florinalexandru302003@gmail.com>
2024-08-12 09:22:40 +0300

    Enhanced design for 'See all history' page

--- lib/components/expense_card.dart
+++ lib/components/expense_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:splitly/models/expense.dart';
+import 'package:splitly/utils.dart';
 
 class ExpenseCard extends StatelessWidget {
   const ExpenseCard({
@@ -11,13 +12,94 @@ class ExpenseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('${expense.expenseName} - ${expense.cost}\$'),
-          Text('(Paid by ${expense.payer})'),
-        ],
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      expense.name,
+                      style: const TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10.0,),
+                    Text(
+                      'Cost: ${expense.cost}\$',
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    const SizedBox(height: 10.0,),
+                    Text(
+                      'Paid by: ${expense.payer}',
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    if (expense.description != null) ...[
+                      const SizedBox(height: 10.0),
+                      Text(
+                        'Description: ${expense.description}',
+                        style: const TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
+              );
+            });
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          elevation: 10.0,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(expense.name),
+                    Text(
+                      formatDate(expense.date),
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Column(children: [
+                  Center(
+                    child: Text(
+                      '${expense.cost}\$',
+                    ),
+                  ),
+                  Text(
+                    '(Paid by ${expense.payer})',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 10,
+                    ),
+                  ),
+                ])
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,22 +1,22 @@
 class Expense {
   Expense({
     this.payer = "You",
-    this.expenseName = "",
+    this.name = "",
     this.cost = 0.0,
     this.description,
   });
 
   String payer;
-  String expenseName;
+  String name;
   double cost;
   DateTime date = DateTime.now();
   String? description;
 }
 
 List<Expense> expensesTest = [
-  Expense(payer: 'You', expenseName: 'Un Pizza Fresco', cost: 24.0),
-  Expense(payer: 'You', expenseName: 'Aventura Park', cost: 54.0),
-  Expense(payer: 'Both', expenseName: 'Costinesti expedition', cost: 345.0),
-  Expense(payer: 'Them', expenseName: 'Munich Trip', cost: -244.0),
-  Expense(payer: 'You', expenseName: 'Germany holiday', cost: 5.0),
+  Expense(payer: 'You', name: 'Un Pizza Fresco', cost: 24.0, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet mauris nec lacus tempus facilisis. Aenean auctor convallis pretium. Nulla a nisl eros. Nullam accumsan venenatis suscipit. Maecenas rhoncus, nisi at semper scelerisque, est arcu mattis dui, eu lobortis quam urna non ipsum. Quisque pharetra venenatis viverra. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam rutrum ligula orci, nec dignissim massa tincidunt eget. Curabitur euismod molestie dolor id feugiat. Aenean sed pellentesque sem.'),
+  Expense(payer: 'You', name: 'Aventura Park', cost: 54.0, description: 'Duis laoreet quis risus ut auctor. Nullam vel feugiat ante, a tempor elit. Aliquam in quam tincidunt, viverra eros in, consequat sem. Vivamus facilisis enim in diam interdum euismod. Aenean id nunc est. Suspendisse quis iaculis libero. Morbi tristique sapien lorem, vitae tempus turpis vestibulum et. Integer eu nisi dapibus, elementum sem quis, volutpat velit. Sed arcu velit, congue ac volutpat at, hendrerit ac metus. Nullam interdum, dui a sodales pharetra, lectus sem blandit odio, sit amet tempus nunc arcu et mi. Mauris hendrerit enim nec ultrices mattis. Praesent sollicitudin in leo vitae hendrerit. Morbi et dapibus dui. '),
+  Expense(payer: 'Both', name: 'Costinesti expedition', cost: 345.0),
+  Expense(payer: 'Them', name: 'Munich Trip', cost: -244.0),
+  Expense(payer: 'You', name: 'Germany holiday', cost: 5.0),
 ];
